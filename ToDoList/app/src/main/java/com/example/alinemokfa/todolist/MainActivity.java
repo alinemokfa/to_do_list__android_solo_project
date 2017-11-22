6eb2798293b69ea713e631d7029e30cbfd05a712
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-11-22 12:55:01 +0000

    added menu and menu item - home

--- ToDoList/app/src/main/java/com/example/alinemokfa/todolist/MainActivity.java
+++ ToDoList/app/src/main/java/com/example/alinemokfa/todolist/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.alinemokfa.todolist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -14,7 +19,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
 
         DatabaseHelper helper = new DatabaseHelper(this);
 
@@ -29,6 +36,30 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Number of tasks: " + tasks.size(), Toast.LENGTH_LONG).show();
 
         }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.main_menu_home){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+//        } else if (item.getItemId() == R.id.action_???) {
+//            Intent intent = new Intent(this, ???Activity.class);
+//            startActivity(intent);
+        }
+        return true;
+    }
+
+    public void getTask(View listItemSelected){
+        Task selectedTask = (Task) listItemSelected.getTag();
+
+        Toast.makeText(this, selectedTask.getTitle(), Toast.LENGTH_SHORT).show();
+    }
 
 
     }
